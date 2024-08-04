ca1f6e01779ee26b7328f9328bc101f0a7f41d71
ijskra <qerman@naver.com>
2024-08-05 01:53:19 +0900

    Update projects.js

--- projects.js
+++ projects.js
@@ -4,28 +4,28 @@ const projectsData = [
         title: "Deluge",
         description: "for orchestra (2024)",
         imageUrl: "pic/deluge1.png",
-        tags: ["orchestra", "contemporary"],
+        tags: ["orchestra", "composition"],
         link: "#"
     },
     {
         title: "Harpsichord Concerto",
         description: "for harpsichord and baroque orchestra (2022)",
         imageUrl: "pic/hpc.jpg",
-        tags: ["harpsichord", "baroque"],
+        tags: ["harpsichord", "baroque", "composition"],
         link: "https://www.youtube.com/watch?v=WuUQeFpRhHo"
     },
     {
         title: "Torso of the Sapphire Wings",
         description: "for two pianos tuned 50 cents apart (2018)",
         imageUrl: "pic/torso1.png",
-        tags: ["piano", "microtonal"],
+        tags: ["piano", "microtonal", "composition"],
         link: "https://soundcloud.com/ijskra/torso-of-the-sapphire-wings-1"
     },
     {
         title: "Romanze",
         description: "for guitar, string quartet and live-electronics (2015)",
         imageUrl: "pic/romanze.png",
-        tags: ["guitar", "electronics"],
+        tags: ["guitar", "electronics", "composition"],
         link: "#"
     }
 ];
