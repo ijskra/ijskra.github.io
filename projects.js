5aa138a91edf397b58e34bbf3295cd11f073a69e
ijskra <qerman@naver.com>
2024-08-05 01:54:16 +0900

    Update projects.js

--- projects.js
+++ projects.js
@@ -11,21 +11,21 @@ const projectsData = [
         title: "Harpsichord Concerto",
         description: "for harpsichord and baroque orchestra (2022)",
         imageUrl: "pic/hpc.jpg",
-        tags: ["harpsichord", "baroque", "composition"],
+        tags: ["harpsichord", "baroque", "composition", "Couperin", "Suite"],
         link: "https://www.youtube.com/watch?v=WuUQeFpRhHo"
     },
     {
         title: "Torso of the Sapphire Wings",
         description: "for two pianos tuned 50 cents apart (2018)",
         imageUrl: "pic/torso1.png",
-        tags: ["piano", "microtonal", "composition"],
+        tags: ["piano", "microtonal", "composition", "quarter tone", "24-EDO"],
         link: "https://soundcloud.com/ijskra/torso-of-the-sapphire-wings-1"
     },
     {
         title: "Romanze",
         description: "for guitar, string quartet and live-electronics (2015)",
         imageUrl: "pic/romanze.png",
-        tags: ["guitar", "electronics", "composition"],
+        tags: ["guitar", "electronics", "composition", "quarter tone", "sixth tone", "36-EDO", "24-EDO"],
         link: "#"
     }
 ];
